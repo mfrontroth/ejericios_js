bdf8fd29645e07eda10880c63ec9f4267616a6a1
martin_frontroth <frontrothm@gmail.com>
2021-11-03 12:39:25 -0300

    excercises 1 - 5

---
+++ actividad_1.js
@@ -0,0 +1,25 @@
+// Actividad Práctica Adicional - JavaScript Unidad 1
+
+// 1. Escribe un programa de una sola línea que haga que aparezca en la pantalla un
+// alert que diga “Hello World”.
+//alert("Hello World");
+
+// 2. Escribe un programa de una sola línea que escriba en la pantalla un texto que
+// diga “Hello World” (document.write).
+//document.write("Hello World");
+
+// 3. Escribe un programa de una sola línea que escriba en la pantalla el resultado
+// de sumar 3 + 5.
+// document.write(3+5);
+
+// 4. Escribe un programa de dos líneas que pida el nombre del usuario con un
+// prompt y escriba un texto que diga “Hola nombreUsuario”
+// var nombre = prompt("Ingrese su nombre: ","");
+// document.write("Hola "+ nombre);
+
+// 5. Escribe un programa de tres líneas que pida un número, pida otro número y
+// escriba el resultado de sumar estos dos números.
+
+// let num1 = prompt("Ingrese un número: ","");
+// let num2 = prompt("Ingrese otro número: ","");
+// document.write("La suma de " + num1 + " y " + num2 + " es: " + (parseInt(num1) + parseInt(num2)));
